0819edf4e0c9396b89be3de6ca75310455896bf3
biakaveron <biakaveron@gmail.com>
2014-01-15 00:17:01 +0400

    updated vk.com OAuth API links

--- classes/kohana/oauth2/provider/vkontakte.php
+++ classes/kohana/oauth2/provider/vkontakte.php
@@ -6,13 +6,12 @@ abstract class Kohana_OAuth2_Provider_VKontakte extends OAuth2_Provider {
 
 	public function url_authorize()
 	{
-		return 'http://api.vkontakte.ru/oauth/authorize';
+		return 'http://oauth.vk.com/authorize';
 	}
 
 	public function url_access_token()
 	{
-		return 'https://api.vkontakte.ru/oauth/access_token';
+		return 'https://oauth.vk.com/access_token';
 	}
-
 
 }
